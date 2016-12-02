cc1c38b41255de141fad3af0a08dd1c57c9c306c
1210265652 <1210265652@qq.com>
2016-12-03 00:12:25 +0800

    mobile2.0

--- js/yindaoye.js
+++ js/yindaoye.js
@@ -10,25 +10,19 @@ window.onload=function() {
         }
         document.getElementsByTagName("html")[0].style.fontSize=scale;
     }
-        resize(750, 'x');
-        var mySwiper = new Swiper('.swiper-container', {
-            loop: true,
+    resize(1334, 'y');
+    var mySwiper = new Swiper('.swiper-container', {
 
-            // 需要分页器
-            pagination: '.swiper-pagination',
 
-            // 前进后退按钮
-            nextButton: '.swiper-button-next',
-            prevButton: '.swiper-button-prev',
+        // 需要分页器
+        pagination: '.swiper-pagination',
 
-            // 滚动条
-            scrollbar: '.swiper-scrollbar',
-            onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
-                swiperAnimateCache(swiper); //隐藏动画元素
-                swiperAnimate(swiper); //初始化完成开始动画
-            },
-            onSlideChangeEnd: function(swiper){
-                swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
-            }
-        })
+        onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
+            swiperAnimateCache(swiper); //隐藏动画元素
+            swiperAnimate(swiper); //初始化完成开始动画
+        },
+        onSlideChangeEnd: function(swiper){
+            swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+        }
+    })
 }
